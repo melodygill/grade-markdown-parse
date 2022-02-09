868d56a8e1530f500e6fa3c0aee90872925c34c7
Melody Gill <melodywgill@gmail.com>
2022-02-09 15:47:31 -0800

    Remove markdown parse folder

--- grade.sh
+++ grade.sh
@@ -1,7 +1,7 @@
 
 git clone $1
-mkdir markdown-parse/
-cp GradingTests.java markdown-parse/
-cd markdown-parse
+#mkdir markdown-parse/
+cp GradingTests.java #markdown-parse/
+#cd markdown-parse
 javac -cp lib/*:. GradingTests.java MarkdownParse.java
 java -cp lib/*:. org.junit.runner.JUnitCore GradingTests
