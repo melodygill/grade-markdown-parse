0e6c53e0399369e0357143022d7ccfbab8bab7dd
Melody Gill <melodywgill@gmail.com>
2022-02-09 15:50:42 -0800

    Change destination

--- grade.sh
+++ grade.sh
@@ -1,7 +1,7 @@
 
 git clone $1
 #mkdir markdown-parse/
-cp GradingTests.java #markdown-parse/
+cp GradingTests.java ~/grade-markdown-parse/CSE15L-Panther #markdown-parse/
 #cd markdown-parse
 javac -cp lib/*:. GradingTests.java MarkdownParse.java
 java -cp lib/*:. org.junit.runner.JUnitCore GradingTests
