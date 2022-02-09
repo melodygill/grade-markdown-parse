669bdd6efa770599788e2c51ff46be07ca12bc41
Melody Gill <melodywgill@gmail.com>
2022-02-09 15:53:05 -0800

    Cd to panther folder

--- grade.sh
+++ grade.sh
@@ -3,5 +3,6 @@ git clone $1
 #mkdir markdown-parse/
 cp GradingTests.java ~/grade-markdown-parse/CSE15L-Panther #markdown-parse/
 #cd markdown-parse
+cd ~/grade-markdown-parse/CSE15L-Panther
 javac -cp lib/*:. GradingTests.java MarkdownParse.java
 java -cp lib/*:. org.junit.runner.JUnitCore GradingTests
